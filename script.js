ed784293ecdea063482939f92227bfc4c34a2c25
Matheus Brian de Oliveira <80044011+matheusbriandeoliveira@users.noreply.github.com>
2023-02-16 18:01:12 -0300

    script e conversoes

--- script.js
+++ script.js
@@ -5,3 +5,21 @@
 // area = altura * comprimento;
 // console.log(area)
 // var area;
+
+// let forma = 'retângulo'
+// let altura = 5;
+// let comprimento = 7;
+// let area;
+
+// if (forma === 'retângulo') {
+//     area = altura * comprimento;
+
+// } else {
+//     area = (altura * comprimento) / 2;
+// }
+
+// console.log(area);
+
+const forma = 'quadrado'
+const altur = 5;
+const comprimento = 7;
